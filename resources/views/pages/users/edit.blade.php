212ec36a56f23c9a04432b232b331cb03e54a3e2
Donboi <79328322+donboi-bae@users.noreply.github.com>
2023-11-19 22:34:12 +0700

    update edit

--- resources/views/pages/users/edit.blade.php
+++ resources/views/pages/users/edit.blade.php
@@ -16,7 +16,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Advanced Forms</h1>
+                <h1>Edit Forms</h1>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Forms</a></div>
